03cc3c796db86911dd82a53d4ae68b7c3c23f7d9
Oracle Public Cloud User <a.filanovskiy@gmail.com>
2020-06-01 22:16:21 +0000

    README changes

--- user-group.tf
+++ user-group.tf
@@ -3,7 +3,8 @@ resource "oci_identity_group" "bds_admin_grp" {
   provider       = oci.home
   name           = "bds_admin_grp"
   description    = "group for bds admins"
-  compartment_id = var.tenancy_ocid
+  // compartment_id = var.tenancy_ocid
+  compartment_id = local.compartment_ocid
   freeform_tags = {
     "environment" = "bds-demo"
   }
@@ -13,8 +14,8 @@ resource "oci_identity_user" "bds_admin_usr" {
   provider       = oci.home
   name           = "bds_admin_usr"
   description    = "user for managing BDS"
-  compartment_id = var.tenancy_ocid
-  // compartment_id = var.compartment_ocid
+  // compartment_id = var.tenancy_ocid
+  compartment_id = local.compartment_ocid
   freeform_tags = {
     "environment" = "bds-demo"
   }
@@ -22,8 +23,8 @@ resource "oci_identity_user" "bds_admin_usr" {
 
 resource "oci_identity_user_group_membership" "user-group-membership" {
   provider       = oci.home
-  compartment_id = var.tenancy_ocid
-  // compartment_id = var.compartment_ocid
+  // compartment_id = var.tenancy_ocid
+  compartment_id = local.compartment_ocid
   user_id  = oci_identity_user.bds_admin_usr.id
   group_id = oci_identity_group.bds_admin_grp.id
 
