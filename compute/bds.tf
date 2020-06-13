0396fb40f88e72c683ea8bf7b5a0d931e858e361
Oracle Public Cloud User <a.filanovskiy@gmail.com>
2020-06-13 00:35:39 +0000

    MVP version

--- compute/bds.tf
+++ compute/bds.tf
@@ -5,7 +5,8 @@ resource oci_bds_bds_instance demo-bds {
   #cluster_public_key = <<Required attribute not found in discovery>>
   cluster_version        = "CDH6"
   compartment_id         = var.compartment_ocid
-  cluster_admin_password = var.bds_instance_cluster_admin_password
+  //cluster_admin_password = var.bds_instance_cluster_admin_password
+  cluster_admin_password = "V2VsY29tZTEjCg=="
   cluster_public_key     = var.ssh_public_key
 
   display_name = "bds-demo"
