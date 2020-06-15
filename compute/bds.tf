934682d421892a5536089b31c220608e12eb628f
Oracle Public Cloud User <a.filanovskiy@gmail.com>
2020-06-15 03:39:43 +0000

    MVP version

--- compute/bds.tf
+++ compute/bds.tf
@@ -6,9 +6,9 @@ resource oci_bds_bds_instance demo-bds {
   cluster_version        = "CDH6"
   compartment_id         = var.compartment_ocid
   //cluster_admin_password = var.bds_instance_cluster_admin_password
-  // "SW5pdDAxIw==", // "Init01#"
+  // "SW5pdDAxIw==", // "Init01#" - works
   // SW5pdDAxMTgwOTEjCg== // Init0118091#
-  // V2VsY29tZTEhCg== // Welcome1!
+  // V2VsY29tZTEhCg== // Welcome1! - doesn't
   // cluster_admin_password = "V2VsY29tZTEhCg=="
   cluster_admin_password = "SW5pdDAxMTgwOTEjCg=="
   cluster_public_key     = var.ssh_public_key
