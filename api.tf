2a23746c3a6f8e991154479214dd0993e2884772
a.filanovskiy@gmail.com <opc@tf-client.bdsnew.bdsvcnnew.oraclevcn.com>
2020-06-17 19:20:21 +0000

    MVP version

--- api.tf
+++ api.tf
@@ -13,7 +13,7 @@ resource "oci_functions_function" "bds-demo-function" {
   #Required
   application_id = oci_functions_application.bds-demo-app.id
   display_name   = "bds-demo-function"
-  image          = "iad.ocir.io/oraclebigdatadb/alexey/hello-java:0.0.2"
+  image          = "iad.ocir.io/oraclebigdatadb/alexey/hello-java:0.0.3"
   memory_in_mbs  = "128"
 
   #Optional
@@ -27,7 +27,7 @@ resource "oci_functions_invoke_function" "bds-demo-function-invoke" {
   function_id = oci_functions_function.bds-demo-function.id
 }
 
-resource "oci_apigateway_gateway" "test_gateway" {
+resource "oci_apigateway_gateway" "bds-demo-gateway" {
   #Required
   compartment_id = local.compartment_ocid
   endpoint_type  = "PUBLIC"
@@ -36,4 +36,28 @@ resource "oci_apigateway_gateway" "test_gateway" {
   #Optional
   display_name  = "bds-demo-api-gw"
   freeform_tags = { "environment" = "bds-demo" }
+}
+
+
+resource "oci_apigateway_deployment" "bds-demo-gw-deployment" {
+    #Required
+    compartment_id = local.compartment_ocid
+    gateway_id = oci_apigateway_gateway.bds-demo-gateway.id
+    path_prefix = "/v1"
+    specification {
+        routes {
+            #Required
+            backend {
+                #Required
+                type = "Oracle Function"
+            }
+            path = "/hello-tf"
+            function_id = oci_functions_function.bds-demo-function.id
+            methods = "GET"
+        }
+    }
+
+    #Optional
+    display_name = "bds-demo-deployment-tf"
+    freeform_tags = { "environment" = "bds-demo" }
 }
