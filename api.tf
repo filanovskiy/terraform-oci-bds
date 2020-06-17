2333ca5bc89892c497f0f395d55acdaedfb535ff
a.filanovskiy@gmail.com <opc@tf-client.bdsnew.bdsvcnnew.oraclevcn.com>
2020-06-17 00:52:50 +0000

    MVP version

--- api.tf
+++ api.tf
@@ -25,3 +25,14 @@ resource "oci_functions_invoke_function" "bds-demo-function" {
     #Required
     function_id = oci_functions_function.test_function.id
 }
+
+resource "oci_apigateway_gateway" "test_gateway" {
+    #Required
+    compartment_id = local.compartment_ocid
+    endpoint_type = "none"
+    subnet_ids = module.vcn.subnet_ids
+
+    #Optional
+    display_name = "bds-demo-api-gw"
+    freeform_tags = {"environment" = "bds-demo"}
+}
