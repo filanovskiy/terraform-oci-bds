19f34cf458e70c316f380a90e5e65a57e2ec7418
a.filanovskiy@gmail.com <opc@tf-client.bdsnew.bdsvcnnew.oraclevcn.com>
2020-06-17 02:50:23 +0000

    ad function policy dependency

--- policies.tf
+++ policies.tf
@@ -8,7 +8,7 @@ resource "oci_identity_policy" allow_bds_read_oci_resources {
   statements = ["allow service bdsprod to {VNIC_READ, VNIC_ATTACH, VNIC_DETACH, VNIC_CREATE, VNIC_DELETE, VNIC_ATTACHMENT_READ, SUBNET_READ, SUBNET_ATTACH, SUBNET_DETACH, INSTANCE_ATTACH_SECONDARY_VNIC, INSTANCE_DETACH_SECONDARY_VNIC} in compartment ${data.oci_identity_compartment.runtime_compartment.name}",
     "allow group ${oci_identity_group.bds_admin_grp.name} to manage bds-instance in compartment ${data.oci_identity_compartment.runtime_compartment.name}",
     "allow group ${oci_identity_group.bds_admin_grp.name} to manage virtual-network-family in compartment ${data.oci_identity_compartment.runtime_compartment.name}",
-    //"allow group ${oci_identity_group.bds_admin_grp.name} to manage repos in tenancy",
+    "allow group ${oci_identity_group.bds_admin_grp.name} to manage repos in compartment ${data.oci_identity_compartment.runtime_compartment.name}",
  //   "allow group ${oci_identity_group.bds_admin_grp.name} to use virtual-network-family in tenancy",
     //"allow group ${oci_identity_group.bds_admin_grp.name} to manage functions-family in tenancy",
     //"allow group ${oci_identity_group.bds_admin_grp.name} to read metrics in tenancy",
