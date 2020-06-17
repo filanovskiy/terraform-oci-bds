539a1384aee19aeb2a7f9a33c61a0f8b93278907
a.filanovskiy@gmail.com <opc@tf-client.bdsnew.bdsvcnnew.oraclevcn.com>
2020-06-17 01:59:06 +0000

    remove bds dependency

--- compute/instanse.tf
+++ compute/instanse.tf
@@ -1,5 +1,5 @@
 resource oci_core_instance bds-demo-egde {
-  depends_on          = [oci_bds_bds_instance.demo-bds]
+  //depends_on          = [oci_bds_bds_instance.demo-bds]
   availability_domain = data.oci_identity_availability_domain.US-ASHBURN-AD-1.name
 
   agent_config {
@@ -57,17 +57,7 @@ resource oci_core_instance bds-demo-egde {
     source      = "./userdata/bootstrap.sh"
     destination = "~/bootstrap.sh"
   }
-  provisioner "file" {
-    connection {
-      agent       = false
-      timeout     = "1m"
-      host        = self.public_ip
-      user        = "opc"
-      private_key = var.ssh_private_key
-    }
-    source      = "./edge_env.sh"
-    destination = "~/edge_env.sh"
-  }
+  
   provisioner "file" {
     connection {
       agent       = false
--- compute/outputs.tf
+++ compute/outputs.tf
@@ -15,29 +15,3 @@ data "oci_core_vnic" "edge_node_vnic" {
 output "public-ip" {
   value = data.oci_core_vnic.edge_node_vnic.public_ip_address
 }
-
-resource "local_file" "edge_env" {
-  content = join("", ["export CLUSTER=${oci_bds_bds_instance.demo-bds.display_name} \n",
-    "export MN0_HOSTNAME=${oci_bds_bds_instance.demo-bds.nodes[0].display_name} \n",
-    "export MN0_IP=${oci_bds_bds_instance.demo-bds.nodes[0].ip_address} \n",
-    "export CM_IP=${substr(oci_bds_bds_instance.demo-bds.cluster_details[0].cloudera_manager_url, 8, length(oci_bds_bds_instance.demo-bds.cluster_details[0].cloudera_manager_url) - 13)} \n",
-    "export CM_ADMIN_USER=admin \n",
-    "export CM_ADMIN_PASSWORD=${base64decode(var.bds_instance_cluster_admin_password)} \n",
-    "export PRIVATE_KEY=/home/opc/.ssh/bdsKey \n",
-    "export SUBNET_CIDR=${data.oci_core_subnet.customer_subnet.cidr_block} \n",
-    "export EDGE_IP=$(hostname -I)  \n",
-    "export EDGE_HOSTNAME=$(hostname -a) \n",
-    "export EDGE_FQDN=$(hostname -A) \n",
-    "export ETC_HOSTS=$EDGE_IP $EDGE_FQDN $EDGE_HOSTNAME",
-    ]
-  )
-  filename = "edge_env.sh"
-}
-
-output "cm_public_ip" {
-  value = oci_core_public_ip.cm_public_ip.ip_address
-}
-
-output "cm_instance_ocid" {
-  value = oci_bds_bds_instance.demo-bds.nodes[2].instance_id
-}
