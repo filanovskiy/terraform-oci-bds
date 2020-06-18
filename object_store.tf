b191dac1b79cd4d8e74f7e483f695aab3a5244b6
a.filanovskiy@gmail.com <opc@tf-client.bdsnew.bdsvcnnew.oraclevcn.com>
2020-06-18 03:35:51 +0000

    MVP version

--- object_store.tf
+++ object_store.tf
@@ -21,7 +21,7 @@ data "oci_objectstorage_namespace" "bds-demo-namespace" {
 resource "oci_objectstorage_preauthrequest" "bds_preauthenticated_request" {
     #Required
     access_type = "AnyObjectWrite"
-    bucket = oci_objectstorage_bucket.tpcds-text-bc.id
+    bucket = oci_objectstorage_bucket.tpcds-text-bc.name
     name = "tpcds-text-pre-auth"
     namespace = data.oci_objectstorage_namespace.bds-demo-namespace.namespace
     time_expires = "2022-08-25T21:10:29.600Z"
