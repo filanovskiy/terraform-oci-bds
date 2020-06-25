be2eefe32af6c5fac76f0ab378f6662a210cbc7a
a.filanovskiy@gmail.com <opc@tf-client.bdsnew.bdsvcnnew.oraclevcn.com>
2020-06-25 04:37:35 +0000

    MVP version

--- compute/instanse.tf
+++ compute/instanse.tf
@@ -90,7 +90,7 @@ resource oci_core_instance bds-demo-egde {
     }
     inline = [
       "chmod +x ~/generate_tpcds_data.sh",
-      "sudo ~/generate_tpcds_data.sh",
+      # "sudo ~/generate_tpcds_data.sh",
       "sudo docker pull msoap/shell2http",
       "sudo docker run -p 8080:8080 --rm -d msoap/shell2http /generate_tpcds_text /home/opc/generate_tpcds_data",
     ]
