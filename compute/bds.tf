867937c981322d3a92115d8c997505611e1cdbd5
a.filanovskiy@gmail.com <a.filanovskiy@gmail.com>
2020-07-22 01:13:00 +0000

    MVP version

--- compute/bds.tf
+++ compute/bds.tf
@@ -68,7 +68,9 @@ resource "null_resource" "remote-exec-un" {
       "sudo docker pull iad.ocir.io/oraclebigdatadb/datageneration/spark-tpcds-gen:latest",
       "sudo docker tag iad.ocir.io/oraclebigdatadb/zeppelin-notebook-bds/zeppelin:latest zeppelin:latest",
       "sudo docker tag iad.ocir.io/oraclebigdatadb/datageneration/spark-tpcds-gen:latest spark-tpcds-gen:latest",
-      "sudo docker run --cpus=4 --memory=12g  -d --network=host --rm -v /opt/:/opt/ -v /etc/hadoop:/etc/hadoop -v /etc/alternatives:/etc/alternatives -v /etc/hive:/etc/hive -v /etc/spark:/etc/spark zeppelin"
+      "sudo docker run --cpus=4 --memory=12g  -d --network=host --rm -v /opt/:/opt/ -v /etc/hadoop:/etc/hadoop -v /etc/alternatives:/etc/alternatives -v /etc/hive:/etc/hive -v /etc/spark:/etc/spark zeppelin",
+      "echo \"* * * * * kinit -kt opc.keytab opc\" >> mycron",
+      "crontab mycron",
     ]
   }
 }
