4a9aa1cb89c260c7ab93f4b20990fc22337bb98f
a.filanovskiy@gmail.com <a.filanovskiy@gmail.com>
2020-08-06 17:43:19 +0000

    MVP version

--- provider.tf
+++ provider.tf
@@ -1,9 +1,11 @@
 provider "oci" {
+  version = "~> 3.88"
   auth   = "InstancePrincipal"
   region = var.region
 }
 
 provider "oci" {
+  version = "~> 3.88"
   auth   = "InstancePrincipal"
   alias  = "home"
   region = var.home_region
