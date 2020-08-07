3e492366e18fd4828a0310356a08dda3b03ac0c9
a.filanovskiy@gmail.com <a.filanovskiy@gmail.com>
2020-08-07 05:43:02 +0000

    MVP version

--- outputs.tf
+++ outputs.tf
@@ -93,8 +93,14 @@ resource "local_file" "bootstrap" {
   filename = "userdata/bootstrap.sh"
 }
 
-resource "local_file" "dcat_create" {
+resource "local_file" "dcat_create_connection" {
+  content = join("", ["{\"isDefault\": \"true\", \"typeKey\": \"cbb356cc-3b00-43c4-b74c-f9f146fc68a9\", \"displayName\": \"obj_st_connection\",\"description\": \"Connection to Obj Store\",\"encProperties\": {\"default\": {}},\"properties\": {\"default\": {\"ociRegion\": \"${var.region}\",\"ociCompartment\": \"${local.compartment_ocid}\"}}}",
+  ])
+  filename = "userdata/dcat/create)connection.json"
+}
+
+resource "local_file" "dcat_create_data_asset" {
   content = join("", ["{\"displayName\": \"BDS_DEMO_DATA_ASSET\",\"typeKey\": \"3ea65bc5-f60d-477a-a591-f063665339f9\",\"properties\": {\"default\": {\"url\": \"https://swiftobjectstorage.${var.region}.oraclecloud.com\",\"namespace\": \"${data.oci_objectstorage_namespace.bds-demo-namespace.namespace}\"}}}",
   ])
-  filename = "userdata/dcat/tf_create.json"
+  filename = "userdata/dcat/create_data_asset.json"
 }
