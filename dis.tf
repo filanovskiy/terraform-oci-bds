7b230d066ea4985f717f9085acc0a48c2c76f9a6
a.filanovskiy@gmail.com <a.filanovskiy@gmail.com>
2020-08-10 17:16:11 +0000

    MVP version

---
+++ dis.tf
@@ -0,0 +1,14 @@
+resource "oci_dataintegration_workspace" "test_workspace" {
+    #Required
+    compartment_id = local.compartment_ocid
+    display_name = "bds-demo-dis"
+
+    #Optional
+      freeform_tags = {"environment" = "bds-demo"}
+    description = "DIS for Big Data Service Demo"
+    //dns_server_ip = "${var.workspace_dns_server_ip}"
+    //dns_server_zone = "${var.workspace_dns_server_zone}"
+    is_private_network_enabled = "false"
+    //subnet_id = "${oci_core_subnet.test_subnet.id}"
+    //vcn_id = "${oci_core_vcn.test_vcn.id}"
+}
