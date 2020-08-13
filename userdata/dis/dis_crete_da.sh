bdf7fa451e7d31c5eca95e79be9bd077f129f712
a.filanovskiy@gmail.com <a.filanovskiy@gmail.com>
2020-08-13 04:55:59 +0000

    MVP version

--- userdata/dis/dis_crete_da.sh
+++ userdata/dis/dis_crete_da.sh
@@ -3,4 +3,4 @@ cp /home/opc/dis/dis_da_create.json /tmp/dis_da_create.json
 sed -i "s/REGION_VAR/$REGION/g" /tmp/dis_da_create.json
 sed -i "s/TENANCY_VAR/$TENANCY_OCID/g" /tmp/dis_da_create.json
 sed -i "s/NAMESPACE_VAR/$OS_NAMESPACE/g" /tmp/dis_da_create.json
-oci data-integration data-asset create --workspace-id $DCAT_OCID --from-json file:///tmp/dis_da_create.json
+oci data-integration data-asset create --workspace-id $DIS_OCID --from-json file:///tmp/dis_da_create.json
